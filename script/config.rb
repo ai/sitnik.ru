5ef1f60e9ec5d3094e49e72c3b74e7877e785c51
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-11 14:37:01 +0400

    Change rsync path on server

--- script/config.rb
+++ script/config.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby1.9
 # Constants and common code for generator and uploader.
 
-UPLOAD_TO = 'sitnik@sitnik.ru:/home/sitnik/data/www/sitnik.ru/'
-
+UPLOAD_TO = 'ai@sitnik.ru:/home/ai/sitnik.ru/'
 
 require 'pathname'
 ROOT    = Pathname.new(__FILE__).dirname.parent.realpath
