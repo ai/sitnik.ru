b30953db28b7eb1683b20589e6b1401d40d73b7e
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-11-08 11:55:03 +0300

    Add i18n support with one layout

--- script/config.rb
+++ script/config.rb
@@ -3,10 +3,29 @@
 UPLOAD_TO = 'ai@sitnik.ru:/home/ai/sitnik.ru/'
 
 require 'pathname'
+require 'rubygems'
+
+gem 'haml'
+require 'haml'
+
 ROOT    = Pathname.new(__FILE__).dirname.parent.realpath
 PUBLIC  = ROOT.join('public')
 CONTENT = ROOT.join('content')
 
 def build
   load Pathname.new(__FILE__).dirname.join('build')
+end
+
+class Translation
+  def initialize(hash)
+    @data = hash
+  end
+  def method_missing(name)
+    value = @data[name.to_s]
+    if value.is_a? Hash
+      Translation.new(value) 
+    else
+      value
+    end
+  end
 end
