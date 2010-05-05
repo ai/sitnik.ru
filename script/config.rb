10a36d4c5f259768c04c796e885cc4b95f7cbd34
Andrey A.I. Sitnik <andrey@sitnik.ru>
2010-05-06 01:36:19 +0400

    Add animation to show avatar

--- script/config.rb
+++ script/config.rb
@@ -26,11 +26,12 @@ class Translation
   def initialize(hash)
     @data = hash
   end
-  def method_missing(name)
+  def method_missing(name, *params)
     value = @data[name.to_s]
     if value.is_a? Hash
       Translation.new(value) 
     else
+      params.each_with_index { |p, i| value = value.gsub("%#{i+1}", p.to_s) }
       value
     end
   end
