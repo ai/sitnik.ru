c60de1bb4a88215f3355a03f9893211db6524c98
Andrey Sitnik <andrey@sitnik.ru>
2019-01-06 22:07:19 +0100

    Fix links rare cases

--- src/index.js
+++ src/index.js
@@ -10,10 +10,3 @@ document.addEventListener('focusin', e => {
     e.target.blur()
   }
 })
-
-document.head.innerHTML += '<style>' +
-  'a::-moz-focus-inner{border: 0}' +
-  '.link{transition:color 200ms}' +
-  '.link:focus{color:white}' +
-  '.link:focus::before{transform:none;background:rgb(24,124,255)}' +
-'</style>'
