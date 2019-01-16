421b025dc1c7a179f8ab27dbb7988f8806dc10ca
Andrey Sitnik <andrey@sitnik.ru>
2019-01-16 15:22:49 +0100

    Get location name

--- src/globe/index.js
+++ src/globe/index.js
@@ -11,11 +11,22 @@ function loadEarth () {
   })
 }
 
+function get (url) {
+  return fetch(url).then(res => res.json())
+}
+
 window.onload = function () {
   Promise.all([
     loadEarth(),
-    fetch('https://evilmartians.com/locations/ai').then(res => res.json())
+    get('https://evilmartians.com/locations/ai')
   ]).then(([showLocation, location]) => {
     showLocation(location)
+    return get('https://maps.googleapis.com/maps/api/geocode/json' +
+      '?latlng=' + location.latitude + ',' + location.longitude +
+      '&language=' + document.documentElement.lang +
+      '&result_type=locality' +
+      '&key=AIzaSyDtN3EGVupACA_bqxQi-5r3iHLCzdeCfZc')
+  }).then(address => {
+    console.log(address.results[0].formatted_address)
   })
 }
