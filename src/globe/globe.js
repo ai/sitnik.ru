4f9d6d1341e3d1bc05c85a8a9960dec34e52cdfd
Andrey Sitnik <andrey@sitnik.ru>
2019-01-21 14:19:20 +0100

    Faster AJAX requests pipeline

--- src/globe/globe.js
+++ src/globe/globe.js
@@ -1,28 +1,32 @@
-function loadEarth () {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement('script')
-    script.async = true
-    script.onerror = reject
-    script.onload = function () {
-      resolve(window.sL)
-    }
-    script.src = document.querySelector('link[as="script"]').href
-    document.head.appendChild(script)
-  })
+function handleError (e) {
+  throw e
 }
 
 function get (url) {
   return fetch(url).then(res => res.json())
 }
 
+function loadEarth () {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script')
+    script.async = true
+    script.onerror = reject
+    script.onload = function () {
+      resolve()
+    }
+    script.src = document.querySelector('link[as="script"]').href
+    document.head.appendChild(script)
+  })
+}
+
+let location
+
 window.onload = function () {
-  Promise.all([
-    loadEarth(),
-    get('https://evilmartians.com/locations/ai')
-  ]).then(loaded => {
-    loaded[0](loaded[1])
+  get('https://evilmartians.com/locations/ai').then(data => {
+    if (window.sL) window.sL(data)
+    location = data
     return get('https://maps.googleapis.com/maps/api/geocode/json' +
-      '?latlng=' + loaded[1].latitude + ',' + loaded[1].longitude +
+      '?latlng=' + data.latitude + ',' + data.longitude +
       '&language=' + document.documentElement.lang +
       '&result_type=locality' +
       '&key=AIzaSyDtN3EGVupACA_bqxQi-5r3iHLCzdeCfZc')
@@ -30,5 +34,9 @@ window.onload = function () {
     let address = geodata.results[0].formatted_address.split(', ')
     document.querySelector('[itemprop=addressLocality]').innerText = address[0]
     document.querySelector('[itemprop=addressCountry]').innerText = address[1]
-  }).catch(e => console.error(e))
+  }).catch(handleError)
+
+  loadEarth().then(() => {
+    if (location) window.sL(location)
+  })
 }
