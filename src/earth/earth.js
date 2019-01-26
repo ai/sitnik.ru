f16f094fc3065b1cd1517df689f5a0da204ec7c2
Andrey Sitnik <andrey@sitnik.ru>
2019-01-26 19:38:35 +0100

    Add controls to earth

--- src/earth/earth.js
+++ src/earth/earth.js
@@ -4,6 +4,7 @@ let { DirectionalLight } = require('three/src/lights/DirectionalLight')
 let { SphereGeometry } = require('three/src/geometries/SphereGeometry')
 let { WebGLRenderer } = require('three/src/renderers/WebGLRenderer')
 let { AmbientLight } = require('three/src/lights/AmbientLight')
+let OrbitControls = require('three-orbitcontrols')
 let { Scene } = require('three/src/scenes/Scene')
 let { Color } = require('three/src/math/Color')
 let { Mesh } = require('three/src/objects/Mesh')
@@ -18,8 +19,12 @@ let div = document.querySelector('.globe_earth')
 let renderer = new WebGLRenderer()
 let scene = new Scene()
 let camera = new PerspectiveCamera(45, 1, 0.01, 1000)
+let controls = new OrbitControls(camera, renderer.domElement)
 
 renderer.setPixelRatio(window.devicePixelRatio)
+
+controls.enableZoom = false
+controls.enableKeys = false
 
 // Light
 
@@ -49,7 +54,9 @@ function resize () {
 }
 
 function render () {
+  controls.update()
   renderer.render(scene, camera)
+  requestAnimationFrame(render)
 }
 
 window.sL = location => {
