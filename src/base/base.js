d47ffaabe076739d82d3b7e2242c289f801244e7
Andrey Sitnik <andrey@sitnik.ru>
2019-01-27 20:40:03 +0100

    Fix pressed style

--- src/base/base.js
+++ src/base/base.js
@@ -6,21 +6,7 @@ document.addEventListener('mousedown', e => {
 })
 
 document.addEventListener('focusin', e => {
-  console.log(e.target, lastDown, Date.now() - lastDownAt)
   if (e.target === lastDown && Date.now() - lastDownAt < 999) {
     e.target.blur()
   }
-})
-
-let lastTouch
-
-document.addEventListener('touchstart', e => {
-  if (e.target.tagName === 'A') {
-    lastTouch = e.target
-    e.target.classList.add('is-pressed')
-  }
-})
-
-document.addEventListener('touchend', () => {
-  lastTouch.classList.remove('is-pressed')
 })
