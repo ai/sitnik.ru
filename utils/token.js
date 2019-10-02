e80175a12ea95aa5a8fd4585fea34710c1d69fc4
Andrey Sitnik <andrey@sitnik.ru>
2019-10-02 23:50:14 +0900

    Use latest location on deploy

--- utils/token.js
+++ utils/token.js
@@ -5,12 +5,11 @@ let readFile = promisify(require('fs').readFile)
 
 let MyError = require('./my-error')
 
-let TOKENS_URL = 'https://console.cloud.google.com/apis/credentials'
 let TOKEN_FILE = join(__dirname, '..', 'token.txt')
 
 async function loadToken () {
   if (!existsSync(TOKEN_FILE)) {
-    throw new MyError(`Save token from ${ TOKENS_URL } to token.txt`)
+    throw new MyError('Decrypt token file with `yarn decrypt`')
   }
   let token = await readFile(TOKEN_FILE)
   return token.toString().trim()
