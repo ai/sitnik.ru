bb9097cce6d77f281d36e01be403c00c94551a95
Andrey Sitnik <andrey@sitnik.ru>
2019-10-11 23:19:06 +0900

    Rename utils/request to utils/get

---
+++ utils/get.js
@@ -0,0 +1,26 @@
+let https = require('https')
+
+let red = require('./red')
+
+function get (url, maxAttempts = 1, attempt = 1) {
+  return new Promise((resolve, reject) => {
+    https.get(url, res => {
+      let buffer = ''
+      res.on('data', i => {
+        buffer += i
+      })
+      res.on('end', () => {
+        resolve(JSON.parse(buffer))
+      })
+    }).on('error', reject)
+  }).catch(e => {
+    if (attempt < maxAttempts) {
+      process.stderr.write(red('E'))
+      return get(url, maxAttempts, attempt + 1)
+    } else {
+      throw e
+    }
+  })
+}
+
+module.exports = get
