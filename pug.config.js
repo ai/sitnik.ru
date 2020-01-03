dc5a52675a40abfad3f62db4551366fc2470ab67
Andrey Sitnik <andrey@sitnik.ru>
2020-01-03 13:34:22 -0800

    Use fake location for tests

--- pug.config.js
+++ pug.config.js
@@ -1,4 +1,17 @@
-let location = require('./src/globe/location.json')
+let { readFileSync, existsSync } = require('fs')
+let { join } = require('path')
+
+let FILE = join(__dirname, 'src', 'globe', 'location.json')
+
+let location = {
+  latitude: 40.7128,
+  longitude: -74.0060,
+  en: { country: 'USA', city: 'New York' },
+  ru: { country: 'США', city: 'Нью-Йорк' }
+}
+if (existsSync(FILE)) {
+  location = JSON.parse(readFileSync(FILE))
+}
 
 module.exports = {
   locals: {
