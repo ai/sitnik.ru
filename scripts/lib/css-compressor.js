ccae1777d1c04d260f57d50604192b3f0905fba1
Andrey Sitnik <andrey@sitnik.ru>
2022-02-10 21:31:34 +0100

    Add Custom Properties compresion

--- scripts/lib/css-compressor.js
+++ scripts/lib/css-compressor.js
@@ -3,22 +3,49 @@ const A = 'a'.charCodeAt(0)
 const COMPRESSED = Symbol('compressed')
 
 export function cssCompressor(classes) {
-  let lastUsed = -1
+  let lastClass = -1
+  let lastCustom = -1
+  let customs = {}
+
   return {
     postcssPlugin: 'cssCompressor',
+
     Rule(rule) {
       if (rule[COMPRESSED]) return
       rule[COMPRESSED] = true
 
-      rule.selector = rule.selector.replace(/\.[\w-]+/g, str => {
-        let kls = str.substr(1)
+      rule.selector = rule.selector.replace(/\.[\w-]+/g, klsSelector => {
+        let kls = klsSelector.substr(1)
         if (!classes[kls]) {
-          lastUsed += 1
-          if (lastUsed === 26) lastUsed -= 26 + 7 + 25
-          classes[kls] = String.fromCharCode(A + lastUsed)
+          lastClass += 1
+          if (lastClass === 26) lastClass -= 26 + 7 + 25
+          classes[kls] = String.fromCharCode(A + lastClass)
         }
         return '.' + classes[kls]
       })
+    },
+
+    Declaration(decl) {
+      if (decl[COMPRESSED]) return
+      decl[COMPRESSED] = true
+
+      if (decl.variable) {
+        if (!customs[decl.prop]) {
+          lastCustom += 1
+          customs[decl.prop] = '--' + String.fromCharCode(A + lastCustom)
+        }
+        decl.prop = customs[decl.prop]
+      } else if (decl.value.includes('var(')) {
+        decl.value = decl.value.replace(/--[\w-]+/, name => {
+          if (!customs[name]) {
+            process.stderr.write(
+              `Custom Properties ${name} without definition\n`
+            )
+            process.exit(1)
+          }
+          return customs[name]
+        })
+      }
     }
   }
 }
